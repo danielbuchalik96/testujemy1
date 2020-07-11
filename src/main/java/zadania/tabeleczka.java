53115bce9d962c74a61df4c9cb6a79074400bc90
uzytkownik <danio9996@o2.pl>
2020-07-11 17:40:30 +0200

    tabeleczkaitestjava

--- src/main/java/zadania/tabeleczka.java
+++ src/main/java/zadania/tabeleczka.java
@@ -1,4 +1,17 @@
 package zadania;
 
 public class tabeleczka {
+    public static void helloUser(String[] users){
+
+        for (String user:users) {
+            System.out.println("hello" + user);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] names = {"Michal", "Sara", "Kamil", "Zaneta", "Lukasz"};
+        helloUser(names);
+    }
+
+
 }
